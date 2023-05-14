e6b81167bdf849afcb53b44825de4825917bd7e2
hyou55 <ihaim733@gmail.com>
2023-05-14 18:29:28 +0900

    DB migration

--- backend/djangoreactapi/post/models.py
+++ backend/djangoreactapi/post/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200) # title 컬럼
-    content = models.TextField()             # content 컬럼
+    sentence = models.TextField()           # title 컬럼
+    meaning = models.TextField()            # content 컬럼
+    # 장고에서 기본으로 제공하는 auth app의 User 모델을 참조하도록 한 것 (블로그 참고)
+    sentence_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+
+
+
 
     def __str__(self):
         """A string representation of the model."""
-        return self.title
+        return self.sentence
 
---
+++ my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gradup88db",  # DB name
+        "USER": "root",  # DB account
+        "PASSWORD": "whybinc2k!",  # DB account's password
+        "HOST": "127.0.0.1",  # DB address(IP)
+        "PORT": "3306",  # DB port(normally 3306)
+    }
+}
