34ea6c7112c630f257ef4f35be21a7e67f74ccac
hyou55 <ihaim733@gmail.com>
2023-05-31 15:06:37 +0900

    Django Google Login Test

--- backend/djangoreactapi/djangoreactapi/settings.py
+++ backend/djangoreactapi/djangoreactapi/settings.py
@@ -20,10 +20,86 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "WMVoca", # app 추가
-    "rest_framework", # rest 추가
-    "corsheaders", # http접근제어 규약해제 명령어추가
+    "django.contrib.sites",
+    
+    # app 추가
+    "WMVoca", 
+
+    # rest 추가
+    "rest_framework",
+    "rest_framework_simplejwt",
+    # 위에꺼 아니면 이걸로 "rest_framework_simplejwt.token_blacklist",
+
+    # dj-rest-auth
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
+
+    # django-allauth
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+
+
+    # http접근제어 규약해제 명령어추가
+    "corsheaders", 
 ]
+
+# 사이트는 하나만 사용할 것이라는 명시
+SITE_ID = 1
+
+# user 앱에서 내가 설정한 User를 사용하겠다고 설정
+AUTH_USER_MODEL = "WMVoca.User"
+
+# dj_rest_auth.registration.views.SocialLoginView를 쓰기 위한 설정
+REST_USE_JWT = True
+
+# 두번째 블로그 참고----------
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+}
+# ----------------------------
+
+
+
+# jwt 토큰은 simplejwt의 JWTAuthentication으로 인증한다.
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+     ] # 이거 소괄호인데 아래 원래 코드에서는 대괄호로 되어있음 혹시 오류나면 변경
+}
+
+
+
+# rest 추가 후 기본 설정
+# REST_FRAMEWORK = {
+#     "DEFAULT_PERMISSION_CLASSES": [
+#         "rest_framework.permissions.AllowAny",
+#     ]
+# }
+
+
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None # username 필드 사용 x
+ACCOUNT_EMAIL_REQUIRED = True            # email 필드 사용 o
+ACCOUNT_USERNAME_REQUIRED = False        # username 필드 사용 x
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+
+# 그외 참조한 블로그에서 있던 설정
+
+# REST_FRAMEWORK = {
+#     "DEFAULT_AUTHENTICATION_CLASSES": (
+#         "rest_framework_simplejwt.authentication.JWTAuthentication",
+#     )
+# }
+
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",     # http접근제어 규약해제 명령어추가
@@ -77,14 +153,6 @@ pymysql.install_as_MySQLdb()
 
 
 
-# rest 추가
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
-    ]
-}
-
-
 # script안에서의 리소스 요청을 허용할 도메인 추가
 # http제한해제 
 CORS_ORIGIN_WHITELIST = [
@@ -129,8 +197,8 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# Google OAuth 관련 설정
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "813909167375-nkh5gpit9r8f59cr8us9sr7pkqnuls0g.apps.googleusercontent.com"
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "AIzaSyBeqDg4SiDQX2CnW3Sg5gTam3VOg7wzgA8"
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ["email"]
-SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {"access_type": "offline"}
+# # Google OAuth 관련 설정
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "813909167375-nkh5gpit9r8f59cr8us9sr7pkqnuls0g.apps.googleusercontent.com"
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "AIzaSyBeqDg4SiDQX2CnW3Sg5gTam3VOg7wzgA8"
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ["email"]
+# SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {"access_type": "offline"}
