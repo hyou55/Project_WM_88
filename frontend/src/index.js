6e4393f2a9c2ab62e92fc23021ed488e30470974
hyou55 <ihaim733@gmail.com>
2023-06-06 01:36:49 +0900

    conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-=======
 import { BrowserRouter } from "react-router-dom";
->>>>>>> 4621dc9e81a9cd43971d6bb4a1f5c66f4a41a474
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
@@ -18,7 +14,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-<<<<<<< HEAD
-
-=======
->>>>>>> 4621dc9e81a9cd43971d6bb4a1f5c66f4a41a474
