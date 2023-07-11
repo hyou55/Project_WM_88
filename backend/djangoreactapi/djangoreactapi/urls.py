5649d09641b2324a7fc3be4f81ff5fc7a46290b1
Jaekyoung-Lee“ <paul_0906@naver.com>
2023-07-11 19:12:05 +0900

    07.11 파파고 api 수정 중

--- backend/djangoreactapi/djangoreactapi/urls.py
+++ backend/djangoreactapi/djangoreactapi/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# 구글 패스로 추가
-from django.urls import path
-from WMVoca.views import *
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('PAPAGO.urls')),
-    # path("app/", include("WMVoca.urls")), # 추가
-
-    # # 구글 패스
-    # path('auth/google/', views.google_auth, name='google-auth'),
-    # path('auth/google/callback/', views.google_auth_callback, name='google-auth-callback'),
-
-
 ]
