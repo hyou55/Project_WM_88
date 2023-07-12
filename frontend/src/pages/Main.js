946fbddcbc07fe05fb7541684c1b043452ebebed
Jaekyoung-Lee“ <paul_0906@naver.com>
2023-07-12 20:37:24 +0900

    파파고 api 적용 완료

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import styles from '../styles/Main.module.css';
+import axios from "axios";
 
 const Main = () => {
+  const [textValue, setTextValue] = useState("");
+  const [resultValue, setResultValue] = useState("");
+
+  const handleSetValue = (e) => {
+    setTextValue(e.target.value);
+  };
+
+  const clicked = () => {
+    axios
+      .post("http://127.0.0.1:8000/", {
+        text: textValue,
+      })
+      .then((response) => {
+        const translatedText = response.data.translated_text;
+        setResultValue(translatedText);
+      })
+      .catch((error) => {
+        console.error(error);
+        setResultValue("번역 실패");
+      });
+  };
   return (
     <div className={styles.mainlayout}>
       <div className={styles.textposition1}>
@@ -13,11 +35,18 @@ const Main = () => {
       <div className={styles.blank0}>
         <hr />
       </div>
-      <input className={styles.inputField}></input>
-        <button className={styles.button}>번역</button>
+      <textarea className={styles.inputField} 
+        placeholder="번역할 내용을 입력하세요."
+        value={textValue}
+        onChange={handleSetValue}
+      ></textarea>
+        <button className={styles.button} onClick={clicked} >번역</button>
       <div className={styles.arrow1}></div>
       <div className={styles.arrow2}></div>
-      <div class={styles.outputbox}></div>
+      <textarea class={styles.outputbox} 
+        placeholder="번역 결과"
+        value={resultValue}
+        readOnly></textarea>
       <div className={styles.blank1}>
         <h4>영어 단어 결과입니다.</h4>
         <h2>단어장에 추가하고 싶은 단어를 선택해주세요.</h2>
