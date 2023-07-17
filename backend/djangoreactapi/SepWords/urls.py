6c2abb8059d56a9a6ab7602a823cec162a40ee4f
hyou55 <ihaim733@gmail.com>
2023-07-17 22:06:12 +0900

    api 적용/ 형태소분석 적용/ 번역 진행중

--- backend/djangoreactapi/SepWords/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.analyze_text, name="analze_text"),
-]
--- backend/djangoreactapi/SepWords/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from nltk import word_tokenize, pos_tag
-
-def analyze_text(request):
-    if request.method == "POST":
-        text = request.POST.get("text", "")
-        tokens = word_tokenize(text)  # 텍스트를 단어로 토큰화
-        analyzed_text = pos_tag(tokens)  # 영어 형태소 분석 수행
-        return JsonResponse({"analyzed_text": analyzed_text})
