f041f7d5950475e6b12b7bf93da490bfe10f56bf
hyou55 <ihaim733@gmail.com>
2023-07-20 14:40:32 +0900

    db 1146오류 관련 urls.py 수정(미해결)

--- backend/djangoreactapi/djangoreactapi/urls.py
+++ backend/djangoreactapi/djangoreactapi/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/WMVoca/", include("allauth.urls")),
     
-    #path("api/WMVoca/", include("WMVoca.urls")),
+    path("api/WMVoca/", include("WMVoca.urls")),
     # 파파고 
     path("api/PAPAGO/", include("PAPAGO.urls")),
     path("api/WMVoca/", include("WMVoca.urls")),
     path("api/process_text/", include("MOR.urls")),
-    # path("app/", include("WMVoca.urls")), # 추가
+    path("app/", include("WMVoca.urls")), # 추가
 
     # # 구글 패스
     # path("auth/google/", views.google_auth, name="google-auth"),
