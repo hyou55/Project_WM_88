623be3e75fa052c4f6a239578a035565e947218f
Jaekyoung-Lee“ <paul_0906@naver.com>
2023-08-31 13:59:08 +0900

    08.31 "텍스트 추출" 버튼 눌렀을 때에 형태소 분석 활성화

--- frontend/src/pages/Image.js
+++ frontend/src/pages/Image.js
@@ -1,43 +1,3 @@
-//import React, { useEffect, useState } from "react";
-//import { createWorker } from 'tesseract.js';
-
-//const Image = () => {
-//  return (
-//    useEffect(async() => {
-//      const worker =await createWorker({
-//        logger: m => console.log(m)
-//      });
-//
-//      (async () => {
-//        await worker.load();
-//        await worker.loadLanguage('eng'); //추출대상 언어
-//        await worker.initialize('eng');	//추출대상 언어
-//        const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-//        console.log(text);
-//        await worker.terminate();
-//      })();
-//  })
-//  );
-//};
-
-//export default Image;
-
-//const [image, setImage] = useState(null);
-//  const [text, setText] = useState(
-//    "현재 클립보드에 저장된 이미지가 없습니다. (No images are currently stored on the clipboard)"
-
-//  async function getImageFromClipboard() {
-//  await navigator.clipboard.readText();
-//  const clipboardItems = await navigator.clipboard.read();
-//  for (const item of clipboardItems) {
-//    for (const type of item.types) {
-//      if (type.startsWith("image/")) {
-//        const blob = await item.getType(type);
-//        if (blob) {
-//          const img = document.createElement("img");
-//          img.src = URL.createObjectURL(blob);
-//          setImage(img);
-
 import React, { useState, useRef } from "react";
 import { createWorker } from "tesseract.js";
 import axios from "axios";
@@ -54,7 +14,7 @@ function Image() {
   const [translate, setTranslate] = useState("");
   const [morResult, setmorResult] = useState([]); // 초기값은 빈 배열로 설정
   const [morTranslate, setmorTranslate] = useState([]); // 초기값은 빈 배열로 설정
-  //const [button2Count, setButton2Count] = useState(0);
+  const [button2Count, setButton2Count] = useState(0);
   const [extractionResult, setExtractionResult] = useState("");
   const [showMorphemeBox, setShowMorphemeBox] = useState(false);
 
@@ -64,29 +24,7 @@ function Image() {
   //if (button2Count%2== 0) clicked();
   //}
 
-  // const convertImageToText = async () => {
-  //   if (!imageData) return;
-  //   const worker = await createWorker({
-  //     logger: (m) => {
-  //       console.log(m);
-  //     },
-  //   });
-  //   await worker.load();
-  //   await worker.loadLanguage("eng");
-  //   await worker.initialize("eng");
-  //   const {
-  //     data: { text },
-  //   } = await worker.recognize(imageData);
-  //   setExtractionResult(text);
-
-  //   //document.getElementById("button2").innerHTML = "한글 번역";
-  //   //button2Count++;
-  // };
-
-  const clicked = async () => {
-    setShowMorphemeBox(true);
-  
-    // 이미지 텍스트 추출
+  const convertImageToText = async () => {
     if (!imageData) return;
     const worker = await createWorker({
       logger: (m) => {
@@ -100,31 +38,34 @@ function Image() {
       data: { text },
     } = await worker.recognize(imageData);
     setExtractionResult(text);
-  
-    // 텍스트 번역
+
+    //document.getElementById("button2").innerHTML = "한글 번역";
+    //button2Count++;
+
+    setShowMorphemeBox(true);
+
     try {
-      const translationResponse = await axios.post("http://127.0.0.1:8000/api/PAPAGO/", {
-        text: text,
-      });
-      const translatedText1 = translationResponse.data.translated_text;
-      setTranslate(translatedText1);
-  
-      const analysisResponse = await axios.post("http://127.0.0.1:8000/api/process_text/", {
-        text: text,
-      });
-      const nouns = analysisResponse.data.nouns;
-  
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/process_text/",
+        {
+          text: text,
+        }
+      );
+
+      const nouns = response.data.nouns;
+
       // 형태소 분석 결과를 words 상태로 업데이트
       setmorTranslate(nouns.map((word) => [word, ""]));
-  
+
       // 형태소 분석 결과를 morp 상태로 업데이트
       setmorResult(nouns);
-  
+
       // 형태소 사전 검색 호출
       const results = [];
       for (const item of nouns) {
-        let dict = "";
+        let dict = ""; // 사전 검색 결과를 담을 변수
         while (dict === "") {
+          // 사전 검색 결과가 빈 문자열일 경우 계속해서 사전 검색 수행
           try {
             dict = await searchDictionary(item);
           } catch (error) {
@@ -132,7 +73,7 @@ function Image() {
             dict = "형태소 사전 검색 실패";
           }
         }
-  
+
         // words 배열의 각 항목에 형태소와 사전 검색 결과를 할당
         setmorTranslate((prevWords) =>
           prevWords.map((word) => {
@@ -142,14 +83,37 @@ function Image() {
             return word;
           })
         );
-  
+
         results.push(dict);
       }
     } catch (error) {
       console.error(error);
-      setmorResult(["처리 실패"]);
+      setmorResult(["형태소 분석 실패"]);
     }
   };
+
+  const clicked = async () => {
+    //document.getElementById("button2").innerHTML = "텍스트 추출";
+    //setButton2Count((prevCount) => prevCount + 1);
+    //button2Count++;
+
+    axios
+      .post("http://127.0.0.1:8000/api/PAPAGO/", {
+        text: extractionResult,
+      })
+      .then((response) => {
+        const translatedText1 = response.data.translated_text;
+        setExtractionResult(translatedText1);
+      })
+      .catch((error) => {
+        console.error(error);
+        setExtractionResult("번역 실패");
+      });
+  };
+  // useEffect(() => {
+  //   convertImageToText();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [imageData]);
 
   function handleImageChange(e) {
     const file = e.target.files[0];
@@ -162,20 +126,6 @@ function Image() {
     };
     reader.readAsDataURL(file);
   }
-  // // Tesseract로 영어 문장을 변환하고, 서버로 전송하는 함수
-  // const sendTextToDjango = async (text) => {
-  //   try {
-  //     const response = await axios.post("/api/process_text/", { text });
-  //     console.log(response.data); // 형태소 분석 결과
-  //     // 분석 결과를 원하는 방식으로 처리
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // // 영어 문장 변환 후 sendTextToDjango 함수 호출 예시
-  // const englishSentence = extractionResult;
-  // sendTextToDjango(englishSentence);
 
   // 형태소 사전 검색 호출
   const searchDictionary = (morp) => {
@@ -198,14 +148,14 @@ function Image() {
     document.getElementById("file-input").click();
   };
 
-  // const button2Switch = async () => {
-  //   if (button2Count % 2 === 0) {
-  //     await convertImageToText();
-  //   } else {
-  //     await clicked();
-  //   }
-  //   setButton2Count((prevCount) => prevCount + 1);
-  // };
+  const button2Switch = async () => {
+    if (button2Count % 2 === 0) {
+      await convertImageToText();
+    } else {
+      await clicked();
+    }
+    setButton2Count((prevCount) => prevCount + 1);
+  };
 
   const morResultRef = useRef(null);
 
@@ -251,9 +201,9 @@ function Image() {
           <button
             id="button2"
             className={styles.button2}
-            onClick={clicked}
+            onClick={button2Switch}
           >
-            {"한글 번역"}
+            {button2Count % 2 === 0 ? "텍스트 추출" : "한글 번역"}
           </button>
 
           {/* <button className={styles.button} onClick={clicked}>
@@ -267,7 +217,7 @@ function Image() {
         <textarea
           className={styles.outputbox}
           placeholder="번역 결과"
-          value={translate}
+          value={extractionResult}
           readOnly
         ></textarea>
       </div>
@@ -308,7 +258,32 @@ function Image() {
               <li>형태소 분석 및 사전 검색 결과</li>
             )}
           </ul>
+          {/* <textarea
+          className={styles.outputbox2}
+          placeholder="형태소 분석 및 사전 검색 결과"
+          //사전 검색 기본 값.
+          value={
+            Array.isArray(morTranslate) && morTranslate.length > 0
+              ? morTranslate
+                  .map((item, index) => {
+                    const analysisResult = item[0];
+                    const dictionaryResult =
+                      Array.isArray(item[1]) &&
+                      item[1].length > 0 &&
+                      item[1][0].length > 0
+                        ? item[1][0][1]
+                        : "";
 
+                    return `${analysisResult}\n${dictionaryResult}\n\n`;
+                  })
+                  .join("")
+              : "형태소 분석 및 사전 검색 결과"
+          }
+          readOnly
+        ></textarea> */}
+
+          {/* 원래 왼쪽에는 형태소 분석, 오른쪽에는 형태소 사전 검색 결과가 있었지만 이제는 형태소, 사전검색 결과가 같이 나오도록 됨. \
+          수정할 것 ->textarea 2개를 하나로 만들고 데이터가 많으면 스크롤로 내리도록 하기 */}
         </div>
       )}
     </div>
